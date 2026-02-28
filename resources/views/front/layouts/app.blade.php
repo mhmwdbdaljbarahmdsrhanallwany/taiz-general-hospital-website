e430484521f13b9b2af9950c36410d7085518b3a
Mahmoud_ALAlwani <mhmwdbdaljbarahmdsrhanallwany@gmail.com>
2026-02-28 03:42:21 +0300

    create page layout

---
+++ resources/views/front/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/front/css/style.css') }}">
+</head>
+<body>
+
+    @include('front.components.topbar')
+    @include('front.components.header')
+
+    @yield('content')
+
+    <div class="overlay" id="overlay"></div>
+
+    <script src="{{ asset('assets/front/js/main.js') }}"></script>
+</body>
+</html>
