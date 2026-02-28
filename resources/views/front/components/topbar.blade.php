57a32b0a90658a7e460eb4e48ff6fad62cfebbca
Mahmoud_ALAlwani <mhmwdbdaljbarahmdsrhanallwany@gmail.com>
2026-02-28 03:43:59 +0300

    create page topbar

---
+++ resources/views/front/components/topbar.blade.php
@@ -0,0 +1,21 @@
+<!-- شريط الطوارئ العلوي -->
+    <div class="top-bar">
+        <div class="top-bar-content">
+            <div class="contact-info-top">
+                <span><i class="fas fa-phone-alt"></i> 781604748</span>
+                <span><i class="fas fa-mobile-alt"></i> 781604747 </span>
+                <span><i class="fas fa-envelope"></i> info@taizhospital.com</span>
+                <span><i class="fas fa-map-marker-alt"></i>العنوان:- التعزية- مفرق ماوية -شارع الثلاثين.</span>
+            </div>
+            <div class="emergency-badge">
+                <i class="fas fa-ambulance"></i> طوارئ 24/7: 04-1234567
+            </div>
+            <div class="social-icons-top">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="fab fa-whatsapp"></i></a>
+            </div>
+        </div>
+    </div>
