bb00154845054234f95f908253c2be73edfd313d
Mahmoud_ALAlwani <mhmwdbdaljbarahmdsrhanallwany@gmail.com>
2026-02-28 03:58:26 +0300

    create page app php

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -1,4 +1,41 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>@yield("title")</title>
+
+    {{-- Font Awoesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    {{-- Goole Font --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+
+    {{-- Css --}}
+    <link rel="stylesheet" href="{{ asset('assets/front/css/style.css') }}">
+
+</head>
+<body>
+
+    {{-- <!-- شريط الطوارئ العلوي --> --}}
+    @include('front.components.topbar')
+     {{-- شريط التنقل  --}}
+    @include("front.components.header")
+
+    {{-- المحتوى الاوسط --}}
+    @yield('content')
+
+    <div class="overlay" id="overlay"></div>
+
+    <script src="{{ asset('assets/front/js/main.js') }}"></script>
+    
+</body>
+</html>
+
+
+{{-- <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
@@ -25,4 +62,4 @@
 
     <script src="{{ asset('assets/front/js/main.js') }}"></script>
 </body>
-</html>
+</html> --}}
